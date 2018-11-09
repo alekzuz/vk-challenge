dde99839232ed104e9a153c984b6ebd5192a2dd4
alekzuz <alekzuz123@gmail.com>
2018-11-09 09:05:16 +0300

    Added empty project

---
+++ VkChallengeFeed/VkChallengeFeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VkChallengeFeed
+//
+//  Created by Aleksei Sakharov on 09/11/2018.
+//  Copyright © 2018 sakharov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
