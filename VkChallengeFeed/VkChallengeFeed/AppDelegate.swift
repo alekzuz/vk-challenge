d8bae6b92820e6c4df23c4a05eb6704a24bb4248
alekzuz <alekzuz123@gmail.com>
2018-11-09 09:32:42 +0300

    Added VkSdk subproject

--- VkChallengeFeed/VkChallengeFeed/AppDelegate.swift
+++ VkChallengeFeed/VkChallengeFeed/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import VKSdkFramework
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        return true
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        VKSdk.processOpen(url, fromApplication: UIApplication.OpenURLOptionsKey.sourceApplication.rawValue)        
         return true
     }
 
